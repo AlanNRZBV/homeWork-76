0afbe51fd4a5b1021e5b6e70505e20043f8d7f8c
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-01-21 16:08:06 +0600

    frontend initial commit

--- api/Data/fileDB.ts
+++ api/Data/fileDB.ts
@@ -1,41 +1,40 @@
-import {promises as fs} from 'fs'
+import { promises as fs } from 'fs';
 import { IMessage, MessageData } from '../types';
-const fileName = './Data/messages/db.json'
+const fileName = './Data/messages/db.json';
 
-let data: IMessage[]=[]
+let data: IMessage[] = [];
 
 export const fileDB = {
-  async init(){
-    try{
-      console.log('catalog: ', process.cwd())
-      const fileContents = await fs.readFile(fileName)
-      data = JSON.parse(fileContents.toString())
-    }catch (e){
-      console.log('Caught on try - INITIALIZE DATA BASE - ', e)
-      data=[]
+  async init() {
+    try {
+      console.log('catalog: ', process.cwd());
+      const fileContents = await fs.readFile(fileName);
+      data = JSON.parse(fileContents.toString());
+    } catch (e) {
+      console.log('Caught on try - INITIALIZE DATA BASE - ', e);
+      data = [];
     }
   },
-  async getItems(){
-    return data.slice(-30)
+  async getItems() {
+    return data.slice(-30);
   },
-  async addItem(item:MessageData){
-
+  async addItem(item: MessageData) {
     const now = new Date();
     const id = crypto.randomUUID();
 
-    const message:IMessage={
-      author:item.author,
-      message:item.message,
+    const message: IMessage = {
+      author: item.author,
+      message: item.message,
       date: now.toISOString(),
-      id:id
-    }
+      id: id,
+    };
 
-    data.push(message)
-    await this.save()
+    data.push(message);
+    await this.save();
 
-    return message
+    return message;
   },
-  async save (){
-    return fs.writeFile(fileName, JSON.stringify(data))
-  }
-}
+  async save() {
+    return fs.writeFile(fileName, JSON.stringify(data));
+  },
+};
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
+import {ThemeProvider} from "@mui/system";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Container, CssBaseline} from "@mui/material";
+import theme from "@/theme";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const queryClient = new QueryClient()
+
+  return (
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline>
+            <html lang="en">
+            <body>
+            <header>
+            </header>
+            <main>
+              <Container maxWidth="xl">
+                {children}
+              </Container>
+            </main>
+            </body>
+            </html>
+          </CssBaseline>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
+}
